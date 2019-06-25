16f2e52bcf4c8ae1dcd868dbb87b5f620b425d5f
Hazel Anne Villareal <hae.villareal@gmail.com>
2019-06-25 22:25:49 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -49,6 +49,10 @@ def add_supply_to_memorial_day(holiday_hash, supply)
       supplies << supply if holiday == :memorial_day
     end
   end
+  
+  holiday_supplies[:spring].map do |holiday, supplies|
+    supplies = supply
+  end
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
